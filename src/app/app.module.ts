eb347bb7837503da964097bebd92b8d75eb1d358
Omar Gutierrez <oaguti@gmail.com>
2021-04-10 12:46:57 -0500

    <refactor>(<elements>): <Creacion de componentes>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DishComponent } from './dish/dish.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DishComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule
