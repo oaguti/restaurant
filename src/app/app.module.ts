e8909daf52ba0fb8018b7a0ae444aeb763b0a937
Omar Gutierrez <oaguti@gmail.com>
2021-04-10 14:17:15 -0500

    build(elements):modify component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { DishComponent } from './dish/dish.component';
 import { MenuComponent } from './menu/menu.component';
+import { IngredientsComponent } from './ingredients/ingredients.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DishComponent,
-    MenuComponent
+    MenuComponent,
+    IngredientsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/ingredients/ingredients.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'og-ingredients',
+  templateUrl: './ingredients.component.html',
+  styleUrls: ['./ingredients.component.scss']
+})
+export class IngredientsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
